bebd53966d609a131dbfedb58fe31717b484ddc9
Satit Rianpit <siteslave@users.noreply.github.com>
2023-03-22 13:28:21 +0700

    fixed: appoint

--- src/tasks.ts
+++ src/tasks.ts
@@ -1,6 +1,8 @@
 import { HealthProfileModel } from './models/health_profile'
+import { UtilsModel } from './models/utils';
 
 const healthProfileModel = new HealthProfileModel()
+const utilsModel = new UtilsModel();
 
 export default async (job: any) => {
   switch (job.name) {
@@ -12,7 +14,23 @@ export default async (job: any) => {
     }
 
     case 'APPOINT': {
-      const data = job.data.appoint
+      const _data = job.data.appoint
+      const tmpData: any[] = utilsModel.appointGroupBy(_data);
+
+      let data: any[] = [];
+      for await (const item of tmpData) {
+        const obj: any = {};
+        obj.hospcode = item.hospcode;
+        obj.hn = item.hn;
+
+        obj.apoint_date = item.apoint_date;
+        obj.appoint_time = item.appoint_time;
+        obj.remark = item.remark;
+        obj.created_at = item.created_at;
+        obj.updated_at = item.updated_at;
+        data.push(obj);
+      }
+
       await healthProfileModel.saveAppoint(data)
       break
     }
